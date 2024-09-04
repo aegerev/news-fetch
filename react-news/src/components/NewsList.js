002b6b4c78d2ac8e896ae8a378328595b26dbcde
alex <alexandersegerev@gmail.com>
2024-09-04 10:27:52 -0400

    Updated NewsList to get the news

--- react-news/src/components/NewsList.js
+++ react-news/src/components/NewsList.js
@@ -4,9 +4,18 @@ import "../App.css"
 
 function newsList(props) {
 
+    const startIdx = (props.pageNo - 1) * props.pageSize;
+    const endIdx = props.pageNo * props.pageSize;
+
+    if(startIdx >= props.articles.length) {
+        return (
+            <p> No news here... 😢</p>
+        );
+    }
+
     const pageArticles = props.articles.slice(startIdx, endIdx);
 
-    const article = pageArticles.map((article) => (
+    const articles = pageArticles.map((article) => (
         <NewsItem key = {article.title} title = {article.title} author= {article.author} description={article.description} url={article.url} />
     ));
 
